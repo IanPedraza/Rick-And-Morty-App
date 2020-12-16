dfba927ab37c6cdd672b571246dcf8022d7de38c
Ian Pedraza <ianpedrazacelon@gmail.com>
2020-12-15 19:35:30 -0600

    presentation in CharactersListFragment

---
+++ app/src/main/kotlin/com/platzi/android/rickandmorty/presentation/CharacterListViewModel.kt
@@ -0,0 +1,109 @@
+package com.platzi.android.rickandmorty.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.platzi.android.rickandmorty.api.*
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class CharacterListViewModel(private val characterRequest: CharacterRequest) : ViewModel() {
+
+    private val disposable = CompositeDisposable()
+
+    private val _events = MutableLiveData<Event<CharacterListNavigation>>()
+    val events: LiveData<Event<CharacterListNavigation>> get() = _events
+
+    private var currentPage = 1
+    private var isLastPage = false
+    private var isLoading = false
+
+    companion object {
+        private const val PAGE_SIZE = 20
+    }
+
+    sealed class CharacterListNavigation {
+        /* AQUI DEFINIMOS LOS EVENTOS DISPONIBLES PARA LA LISTA DE PERSONAJES */
+
+        /* Estos eventos devuelven datos, por lo que los creamo como data class */
+        data class ShowCharacterError(val error: Throwable) : CharacterListNavigation()
+        data class ShowCharacterList(val characterList: List<CharacterServer>) :
+            CharacterListNavigation()
+
+        /* Los siguiente eventos no devuelven datos, por lo que los definiremos como Objects */
+        object HideLoading : CharacterListNavigation()
+        object ShowLoading : CharacterListNavigation()
+    }
+
+    fun onLoadMoreItems(visibleItemCount: Int, firstVisibleItemPosition: Int, totalItemCount: Int) {
+        if (isLoading || isLastPage || !isInFooter(
+                visibleItemCount,
+                firstVisibleItemPosition,
+                totalItemCount
+            )
+        ) {
+            return
+        }
+
+        currentPage += 1
+        onGetAllCharacters()
+    }
+
+    private fun isInFooter(
+        visibleItemCount: Int,
+        firstVisibleItemPosition: Int,
+        totalItemCount: Int
+    ): Boolean {
+        return visibleItemCount + firstVisibleItemPosition >= totalItemCount
+                && firstVisibleItemPosition >= 0
+                && totalItemCount >= PAGE_SIZE
+    }
+
+    fun onRetryGetAllCharacter(itemCount: Int) {
+        if (itemCount > 0) {
+            //srwCharacterList.isRefreshing = false
+            _events.value = Event(CharacterListNavigation.HideLoading)
+            return
+        }
+
+        onGetAllCharacters()
+    }
+
+    fun onGetAllCharacters() {
+        disposable.add(
+            characterRequest
+                .getService<CharacterService>()
+                .getAllCharacters(currentPage)
+                .map(CharacterResponseServer::toCharacterServerList)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .doOnSubscribe {
+                    //srwCharacterList.isRefreshing = true
+                    _events.value = Event(CharacterListNavigation.ShowLoading)
+                }
+                .subscribe({ characterList ->
+                    if (characterList.size < PAGE_SIZE) {
+                        isLastPage = true
+                    }
+
+                    //srwCharacterList.isRefreshing = false
+                    //characterGridAdapter.addData(characterList)
+                    _events.value = Event(CharacterListNavigation.HideLoading)
+                    _events.value = Event(CharacterListNavigation.ShowCharacterList(characterList))
+                }, { error ->
+                    isLastPage = true
+                    //srwCharacterList.isRefreshing = false
+                    //context?.showLongToast("Error -> ${error.message}")
+                    _events.value = Event(CharacterListNavigation.HideLoading)
+                    _events.value = Event(CharacterListNavigation.ShowCharacterError(error))
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+
+}
